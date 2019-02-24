3b96b1f8494dd692cc77f645b795a8ac746475a6
freeday <seekwell@gmail.com>
2019-02-24 20:07:49 +0300

    webpackconfig

---
+++ webpack.config.js
@@ -0,0 +1,96 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+let conf = {
+  entry: {
+    app: path.join(__dirname, './src')
+  },
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, './dist'),
+    publicPath: '/dist/'
+  },
+  devServer: {
+    overlay: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              // publicPath: 'fonts',
+              outputPath: 'fonts',
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|svg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'img',
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    }),
+    new HtmlWebpackPlugin({
+      hash: false,
+      template: './index.html',
+      filename: './index.html'
+    }),
+    new CopyWebpackPlugin([
+      {from: `${path.join(__dirname, './src')}/img/`, to: `${path.join(__dirname, './dist/img/')}`}
+    ])
+  ]
+}
+
+module.exports = (env, options) => {
+  conf.devtool = (options.mode === 'production') ? false : 'eval-sourcemap'
+  return conf
+}
