0a6d36c243080b6fed2e78369b4f379cdb1e394a
freeday <seekwell@gmail.com>
2019-02-24 20:49:21 +0300

    [+] validate form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import $ from 'jquery'
+import './js/validate'
 import './scss/main.scss'
+
+$(window).on('load', function () {
+  $('#btnSbmt').on('click', function(e) {
+    e.preventDefault()
+    if ($form.validateForm()) {
+      $(this).off()
+      $input.off()
+      $form.submit()
+    }
+  })
+});
